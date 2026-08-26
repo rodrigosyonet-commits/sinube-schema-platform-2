15c89b793fedf49210c0f19e978cc5f61339e5b9
rodrigosyonet-commits <rodrigo.syonet@gmail.com>
2026-08-26 10:52:19 -0600

    Add EntityExtractor class to fetch entities

--- src/extractors/entity-extractor.ts
+++ src/extractors/entity-extractor.ts
@@ -1,1 +1,29 @@
 
+import { SinubeClient } from "../client/sinube-client.js";
+import { parseSiNube } from "../parser/parse-sinube.js";
+
+export class EntityExtractor {
+
+  constructor(
+    private readonly client: SinubeClient
+  ) {}
+
+  async execute(): Promise<string[]> {
+
+    const sql = `
+      SELECT entidad
+      FROM DbEntidad
+      TAMPAG 999
+    `;
+
+    const raw =
+      await this.client.execute(sql);
+
+    const data =
+      parseSiNube(raw);
+
+    return data.rows.map(
+      r => r.entidad
+    );
+  }
+}
